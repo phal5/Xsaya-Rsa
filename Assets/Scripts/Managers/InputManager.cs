e330e01dc7a3a49062909aa8fffa65fa11de3aa7
phal5 <77428425+phal5@users.noreply.github.com>
2026-05-11 21:09:07 +0900

    Managers

---
+++ Assets/Scripts/Managers/InputManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] KeyCode _attack;
+    [SerializeField] KeyCode _jump;
+
+    [SerializeField] KeyCode left;
+    [SerializeField] KeyCode right;
+    [SerializeField] KeyCode up;
+    [SerializeField] KeyCode down;
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Rewire()
+    {
+
+    }
+}
