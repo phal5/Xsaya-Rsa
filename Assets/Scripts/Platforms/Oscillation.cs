2b757db811d681ea231e46f15ff852f596ee8ed8
phal5 <77428425+phal5@users.noreply.github.com>
2026-09-06 18:54:44 +0900

    chore : Namespace addition for Disambiguation

--- Assets/Scripts/Platforms/Oscillation.cs
+++ Assets/Scripts/Platforms/Oscillation.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
 
+namespace MovingPlatforms
+{
+
 public class Oscillation : MonoBehaviour
 {
     [SerializeField] Vector3 _point1;
@@ -9,8 +12,10 @@ public class Oscillation : MonoBehaviour
     [SerializeField] Rigidbody _rigidbody;
 
     float _t = 0;
-    float _prevT = 0;
 
+    // 가정: 발판은 한 프레임만에 한 주기를 초과한 분량을 이동하지 않을 것이다
+    // * 만일 프레임 한 번에 걸쳐 이동 발판이 한 주기(원래 위치로 돌아오는 시간) 이상을 움직인다면 그 컴퓨터로는 이 게임을 가동해서는 안 된다
+    
     private void FixedUpdate()
     {
         _t += Time.fixedDeltaTime * _frequency;
@@ -19,4 +24,6 @@ public class Oscillation : MonoBehaviour
         Vector3 position = Vector3.Lerp(_point1, _point2, t);
         _rigidbody.MovePosition(position);
     }
+}
+
 }
